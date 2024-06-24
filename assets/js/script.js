fcb0bae7265bb74fd373e438613af9e1f9008c92
Bella Matasic <bella.matasic@gmail.com>
2024-06-24 14:55:03 -0400

    Fix: Navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// import velocity from 'velocity-animate';
+
 $(document).ready(function () {
   $(window).scroll(function () {
     var scroll = $(window).scrollTop();
@@ -100,5 +102,5 @@ $(document).ready(function () {
       $("#" + target).addClass("active");
     });
   });
-  
+
 });
